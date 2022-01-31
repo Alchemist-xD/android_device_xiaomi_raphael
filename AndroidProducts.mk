0c5810ebda2b2dcc15230644a76522d591f24d6b
Alchemist-xD <98739960+Alchemist-xD@users.noreply.github.com>
2022-01-31 17:41:58 +0530

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/cipher_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    cipher_raphael-user \
+    cipher_raphael-userdebug \
+    cipher_raphael-eng
---
+++ cipher_raphael.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from raphael device
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some common Cipher stuff.
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
+
+# Cipher Flags
+CIPHER_MAINTAINER := Alchemist
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := cipher_raphael
+PRODUCT_DEVICE := raphael
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi K20 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
